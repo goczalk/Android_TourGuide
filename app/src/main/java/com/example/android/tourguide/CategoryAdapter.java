d08e65393646e30885b8952d0c513b8247793776
Klaudia Goczal <klaudia@lodz.pl>
2018-07-29 21:23:17 +0200

    First attempt to TabView

---
+++ app/src/main/java/com/example/android/tourguide/CategoryAdapter.java
@@ -0,0 +1,44 @@
+package com.example.android.tourguide;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by klaudia on 29/07/18.
+ */
+
+class CategoryAdapter extends FragmentPagerAdapter  {
+
+    public static final int PAGE_COUNT = 4;
+    private Context mContext;
+
+    public CategoryAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_events);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_restaurants);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_museums);
+        } else {
+            return mContext.getString(R.string.category_entertainment);
+        }
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PageFragment.newInstance(position);
+    }
+}
